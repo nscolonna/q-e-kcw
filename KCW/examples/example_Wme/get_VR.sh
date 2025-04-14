b39a3b4bba348ef31f6db0695229ac01cc556ac0
ncolonna <nicola.colonna@psi.ch>
2025-04-14 17:37:31 +0200

    Update script to get on site screened coulomb as a funciton of R

--- KCW/examples/example_Wme/get_VR.sh
+++ KCW/examples/example_Wme/get_VR.sh
@@ -23,13 +23,13 @@ done
 # <ij|V|kl> = \int dr1 dr2 i(r1)*j(r2) v(r1,r2) k(r2)l(r1)
 # at the moment only <ij|V|ji> = <rho_i|V|rho_j> = int dr1 dr2 \rho(r1) v(r1,r2) \rho(r2) 
 for j in `seq 1 8`; do 
-grep "  1           $j           1" barecoulomb.txt  |awk '{print $4, $5}' | sed -r '/^\s*$/d' > pp
+grep "    1    $j    1    " barecoulomb.txt  |awk '{print $4, $5}' | sed -r '/^\s*$/d' > pp
 paste R.dat pp | awk '{printf "%12.8f %21.18f %21.18f %5i %5i %5i\n", sqrt($1*$1+$2*$2+$3*$3), $4, $5, $1, $2, $3}'> V1${j}bare.dat
 sort -n V1${j}bare.dat > pp 
 mv pp V1${j}bare.dat
 sed -i '1 i\#   |R|             Re[V]                Im[V]              R1    R2    R3' V1${j}bare.dat
 
-grep "  1           $j           1" screencoulomb.txt  |awk '{print $4, $5}' | sed -r '/^\s*$/d' > pp
+grep "    1    $j    1    " screencoulomb.txt  |awk '{print $4, $5}' | sed -r '/^\s*$/d' > pp
 paste R.dat pp | awk '{printf "%12.8f %21.18f %21.18f %5i %5i %5i\n", sqrt($1*$1+$2*$2+$3*$3), $4, $5, $1, $2, $3}'> V1${j}screen.dat
 sort -n V1${j}screen.dat > pp 
 mv pp V1${j}screen.dat
