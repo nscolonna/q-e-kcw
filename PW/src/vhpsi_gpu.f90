e4f17cdb26cbc03a18013f9cce19397f5b698a72
Paolo Giannozzi <p.giannozzi@gmail.com>
2025-09-22 22:07:16 +0200

    More DFT+U cleanup There is a single call to vhpsi in h_psi; fixes clocks as well

--- PW/src/vhpsi_gpu.f90
+++ PW/src/vhpsi_gpu.f90
@@ -39,8 +39,6 @@ SUBROUTINE vhpsi_gpu( ldap, np, mps, psip, hpsi )
   !
   IF ( .NOT. ANY(is_hubbard(:)) .AND. .NOT.ANY(is_hubbard_back(:)) ) RETURN
   !
-  CALL start_clock( 'vhpsi' )
-  !
   !$acc data present(wfcU)
   !
   IF (gamma_only) THEN
@@ -50,8 +48,6 @@ SUBROUTINE vhpsi_gpu( ldap, np, mps, psip, hpsi )
   ENDIF
   !
   !$acc end data
-  !
-  CALL stop_clock( 'vhpsi' )
   !
   RETURN
   !
