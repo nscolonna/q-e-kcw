78fdb0bfa69c1d3f159172f43d8d59624f711ae8
Harmen Stoppels <me@harmenstoppels.nl>
2026-05-07 11:58:19 +0200

    cmake: reproducible install of Fortran modules
    qe_install_targets installs Fortran module files for all target types,
    including executables, to the same destination `include/qe/<lib>/`.
    
    A few executables define the same Fortran module ifconstants, in ZG.x
    and disca.x. That results in different .mod files installed to the
    same location. They differ only by the GCC header.
    
    I think it doesn't make sense to install mod files for executables, so
    that seems to be the easiest fix.
    
    While at it, fix the comment/workaround for CMake < 3.19, because QE
    requires CMake 3.20+.
    
    Signed-off-by: Harmen Stoppels <me@harmenstoppels.nl>

--- cmake/qeHelpers.cmake
+++ cmake/qeHelpers.cmake
@@ -55,8 +55,8 @@ function(qe_fix_fortran_modules TGT)
     set(targets ${TGT} ${ARGN})
     foreach(tgt IN LISTS targets)
         get_target_property(tgt_type ${tgt} TYPE)
-        # All of the following target modifications make
-        # sense on non-interfaces only
+        # Interface libraries compile no sources, so module-related
+        # properties do not apply to them.
         if(NOT ${tgt_type} STREQUAL "INTERFACE_LIBRARY")
             get_target_property(tgt_module_dir ${tgt} Fortran_MODULE_DIRECTORY)
             # set module path to tgt_binary_dir/mod
@@ -211,13 +211,9 @@ function(qe_install_targets TGT)
         LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
         RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR} # Windows needs RUNTIME also for libraries
         PUBLIC_HEADER DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/qe/${TGT})
-    # Retrieving non-whitelisted properties leads to an hard
-    # error, let's skip the following section for interface
-    # targets. See here for details:
-    # https://gitlab.kitware.com/cmake/cmake/issues/17640
     foreach(tgt IN LISTS targets)
         get_target_property(tgt_type ${tgt} TYPE)
-        if(NOT ${tgt_type} STREQUAL "INTERFACE_LIBRARY")
+        if(NOT ${tgt_type} STREQUAL "EXECUTABLE")
             # If the target generates Fortran modules, make sure
             # to install them as well to a proper location
             get_target_property(tgt_module_dir ${tgt} Fortran_MODULE_DIRECTORY)
@@ -225,7 +221,7 @@ function(qe_install_targets TGT)
                 install(DIRECTORY ${tgt_module_dir}/
                     DESTINATION ${QE_INSTALL_Fortran_MODULES}/qe/${TGT})
             endif()
-        endif()        
+        endif()
     endforeach()
 endfunction(qe_install_targets)
 
