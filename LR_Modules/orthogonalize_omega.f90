4aead32939542413d231f83f56f71040a8b87c94
Oscar Baseggio <obaseggi@bionano2.dimi.uniud.it>
2026-08-31 15:16:59 +0200

    fix eels uspp tests

--- LR_Modules/orthogonalize_omega.f90
+++ LR_Modules/orthogonalize_omega.f90
@@ -27,7 +27,7 @@ USE becmod,      ONLY : bec_type, becp, calbec
 USE uspp,        ONLY : vkb, okvan
 USE mp_bands,    ONLY : intra_bgrp_comm
 USE mp,          ONLY : mp_sum
-USE control_flags, ONLY : gamma_only
+USE control_flags, ONLY : gamma_only, offload_type
 USE gvect,       ONLY : gstart
 !
 IMPLICIT NONE
@@ -41,21 +41,30 @@ COMPLEX(DP), INTENT(IN) :: w
 
 COMPLEX(DP), ALLOCATABLE :: ps(:,:)
 REAL(DP), ALLOCATABLE :: ps_r(:,:)
+COMPLEX(DP), ALLOCATABLE :: wwg(:)
 
 INTEGER :: ibnd, jbnd, nbnd_eff
 REAL(DP) :: wg1, w0g, wgp, deltae, theta
 REAL(DP), EXTERNAL :: w0gauss, wgauss
-COMPLEX(DP) :: wwg
 ! functions computing the delta and theta function
 
 CALL start_clock ('ortho')
 IF (gamma_only) THEN
    ALLOCATE(ps_r(nbnd,nbnd))
+   !$acc enter data create(ps_r(1:nbnd,1:nbnd))
+   !$acc kernels present(ps_r)
    ps_r = 0.0_DP
+   !$acc end kernels
 ENDIF
 
 ALLOCATE(ps(nbnd,nbnd))
+ALLOCATE(wwg(nbnd))
+
+!$acc data present_or_copyin(evq) present_or_copy(dvpsi,dpsi) create(ps(1:nbnd,1:nbnd))
+
+!$acc kernels present(ps)
 ps = (0.0_DP, 0.0_DP)
+!$acc end kernels
 
 !
 if (lgauss) then
@@ -65,13 +74,15 @@ if (lgauss) then
    !
    !  metallic case
    !
+   !$acc host_data use_device(evq, dvpsi, ps)
    IF (noncolin) THEN
-      CALL zgemm( 'C', 'N', nbnd, nbnd_occ (ikk), npwx*npol, (1.d0,0.d0), &
+      CALL myzgemm( 'C', 'N', nbnd, nbnd_occ (ikk), npwx*npol, (1.d0,0.d0), &
                  evq, npwx*npol, dvpsi, npwx*npol, (0.d0,0.d0), ps, nbnd )
    ELSE
-      CALL zgemm( 'C', 'N', nbnd, nbnd_occ (ikk), npwq, (1.d0,0.d0), &
+      CALL myzgemm( 'C', 'N', nbnd, nbnd_occ (ikk), npwq, (1.d0,0.d0), &
                  evq, npwx, dvpsi, npwx, (0.d0,0.d0), ps, nbnd )
    END IF
+   !$acc end host_data
    !
 
    DO ibnd = 1, nbnd_occ (ikk)
@@ -81,18 +92,26 @@ if (lgauss) then
          wgp = wgauss ( (ef - et (jbnd, ikq) ) / degauss, ngauss)
          deltae = et (jbnd, ikq) - et (ibnd, ikk)
          theta = wgauss (deltae / degauss, 0)
-         wwg = CMPLX(wg1 * (1.d0 - theta) + wgp * theta,0.0_DP)
+         wwg(jbnd) = CMPLX(wg1 * (1.d0 - theta) + wgp * theta,0.0_DP)
          IF (jbnd <= nbnd_occ (ikq) ) THEN
-            wwg = wwg + alpha_pv * theta * (wgp - wg1) / (deltae - w)
+            wwg(jbnd) = wwg(jbnd) + alpha_pv * theta * (wgp - wg1) / (deltae - w)
          ENDIF
-         !
-         ps(jbnd,ibnd) = wwg * ps(jbnd,ibnd)
-         !
       ENDDO
+      !
+      !$acc parallel loop copyin(wwg) present(ps)
+      DO jbnd = 1, nbnd
+         ps(jbnd,ibnd) = wwg(jbnd) * ps(jbnd,ibnd)
+      ENDDO
+      !$acc end parallel loop
+      !
       IF (noncolin) THEN
-         CALL dscal (2*npwx*npol, wg1, dvpsi(1,ibnd), 1)
+         !$acc kernels present(dvpsi)
+         dvpsi(1:npwx*npol, ibnd) = wg1 * dvpsi(1:npwx*npol, ibnd)
+         !$acc end kernels
       ELSE
-         call dscal (2*npwq, wg1, dvpsi(1,ibnd), 1)
+         !$acc kernels present(dvpsi)
+         dvpsi(1:npwq, ibnd) = wg1 * dvpsi(1:npwq, ibnd)
+         !$acc end kernels
       END IF
    END DO
    nbnd_eff=nbnd
@@ -100,48 +119,63 @@ ELSE
    !
    !  insulators
    !
+   !$acc host_data use_device(evq, dvpsi, ps)
    IF (noncolin) THEN
-      CALL zgemm( 'C', 'N',nbnd_occ(ikq), nbnd_occ(ikk), npwx*npol, &
+      CALL myzgemm( 'C', 'N',nbnd_occ(ikq), nbnd_occ(ikk), npwx*npol, &
              (1.d0,0.d0), evq, npwx*npol, dvpsi, npwx*npol, &
              (0.d0,0.d0), ps, nbnd )
    ELSEIF (gamma_only) THEN
-            CALL dgemm( 'C', 'N', nbnd_occ(ikq), nbnd_occ (ikk), 2*npwq, &
+        !$acc host_data use_device(ps_r)
+            CALL mydgemm( 'C', 'N', nbnd_occ(ikq), nbnd_occ (ikk), 2*npwq, &
              2.0_DP, evq, 2*npwx, dvpsi, 2*npwx, &
              0.0_DP, ps_r, nbnd )
             IF (gstart == 2 ) THEN
-               CALL DGER( nbnd_occ(ikq), nbnd_occ (ikk), -1.0_DP, evq, &
+               CALL mydger( nbnd_occ(ikq), nbnd_occ (ikk), -1.0_DP, evq, &
                     & 2*npwq, dvpsi, 2*npwx, ps_r, nbnd )
             ENDIF
+        !$acc end host_data
    ELSE
-      CALL zgemm( 'C', 'N', nbnd_occ(ikq), nbnd_occ (ikk), npwq, &
+      CALL myzgemm( 'C', 'N', nbnd_occ(ikq), nbnd_occ (ikk), npwq, &
              (1.d0,0.d0), evq, npwx, dvpsi, npwx, &
              (0.d0,0.d0), ps, nbnd )
    END IF
+   !$acc end host_data
    nbnd_eff=nbnd_occ(ikk)
 END IF
 IF (gamma_only) THEN
+   !$acc host_data use_device(ps_r)
    call mp_sum(ps_r(:,:),intra_bgrp_comm)
+   !$acc end host_data
 ELSE
+   !$acc host_data use_device(ps)
    call mp_sum(ps(:,1:nbnd_eff),intra_bgrp_comm)
+   !$acc end host_data
 ENDIF
 !
 ! dpsi is used as work space to store S|evc>
 !
-IF (okvan) CALL calbec ( npwq, vkb, evq, becp, nbnd_eff)
+IF (okvan) CALL calbec ( offload_type, npwq, vkb, evq, becp, nbnd_eff)
 CALL s_psi (npwx, npwq, nbnd_eff, evq, dpsi)
 !
 ! |dvspi> =  -(|dvpsi> - S|evq><evq|dvpsi>)
 !
+IF (gamma_only) THEN
+   !$acc kernels present(ps)
+   ps = CMPLX (ps_r,0.0_DP, KIND=DP)
+   !$acc end kernels
+ENDIF
+!
+!$acc host_data use_device(dpsi, ps, dvpsi)
 if (lgauss) then
    !
    !  metallic case
    !
    IF (noncolin) THEN
-      CALL zgemm( 'N', 'N', npwx*npol, nbnd_occ(ikk), nbnd, &
+      CALL myzgemm( 'N', 'N', npwx*npol, nbnd_occ(ikk), nbnd, &
                 (1.d0,0.d0), dpsi, npwx*npol, ps, nbnd, (-1.0d0,0.d0), &
                 dvpsi, npwx*npol )
    ELSE
-      CALL zgemm( 'N', 'N', npwq, nbnd_occ(ikk), nbnd, &
+      CALL myzgemm( 'N', 'N', npwq, nbnd_occ(ikk), nbnd, &
              (1.d0,0.d0), dpsi, npwx, ps, nbnd, (-1.0d0,0.d0), &
               dvpsi, npwx )
    END IF
@@ -150,21 +184,28 @@ ELSE
    !  Insulators: note that nbnd_occ(ikk)=nbnd_occ(ikq) in an insulator
    !
    IF (noncolin) THEN
-      CALL zgemm( 'N', 'N', npwx*npol, nbnd_occ(ikk), nbnd_occ(ikk), &
+      CALL myzgemm( 'N', 'N', npwx*npol, nbnd_occ(ikk), nbnd_occ(ikk), &
                 (1.d0,0.d0),dpsi,npwx*npol,ps,nbnd,(-1.0d0,0.d0), &
                 dvpsi, npwx*npol )
    ELSEIF (gamma_only) THEN
-      ps = CMPLX (ps_r,0.0_DP, KIND=DP)
-      CALL ZGEMM( 'N', 'N', npwq, nbnd_occ(ikk), nbnd_occ(ikk), &
+      CALL myzgemm( 'N', 'N', npwq, nbnd_occ(ikk), nbnd_occ(ikk), &
                (1.d0,0.d0), dpsi, npwx, ps, nbnd, (-1.0d0,0.d0), &
                 dvpsi, npwx )
    ELSE
-      CALL zgemm( 'N', 'N', npwq, nbnd_occ(ikk), nbnd_occ(ikk), &
+      CALL myzgemm( 'N', 'N', npwq, nbnd_occ(ikk), nbnd_occ(ikk), &
              (1.d0,0.d0), dpsi, npwx, ps, nbnd, (-1.0d0,0.d0), &
               dvpsi, npwx )
    END IF
 ENDIF
+!$acc end host_data
+!
+!$acc end data
 
+IF (gamma_only) THEN
+   !$acc exit data delete(ps_r)
+   DEALLOCATE(ps_r)
+ENDIF
+DEALLOCATE(wwg)
 DEALLOCATE(ps)
 CALL stop_clock ('ortho')
 RETURN
